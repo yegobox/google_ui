574c10426f3d6764ec50bcc12da6d62bc80c971d
Ed Sulaiman <52323183+edsulaiman@users.noreply.github.com>
2021-05-16 17:07:49 +0700

    add: custom property to drawer

--- lib/src/widgets/google_drawer.dart
+++ lib/src/widgets/google_drawer.dart
@@ -5,26 +5,31 @@ class GoogleDrawer extends StatelessWidget {
     Key? key,
     this.backgroundColor,
     required this.children,
+    this.custom = false,
   }) : super(key: key);
 
   final Color? backgroundColor;
   final List<Widget> children;
+  final bool custom;
 
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
 
     return Theme(
-      data: Theme.of(context)
-          .copyWith(canvasColor: backgroundColor ?? colorScheme.surface),
+      data: Theme.of(context).copyWith(
+        canvasColor: backgroundColor ?? colorScheme.surface,
+      ),
       child: Drawer(
-        child: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              children: children,
-            ),
-          ),
-        ),
+        child: custom
+            ? SafeArea(
+                child: SingleChildScrollView(
+                  child: Column(children: children),
+                ),
+              )
+            : Column(
+                children: children,
+              ),
       ),
     );
   }
