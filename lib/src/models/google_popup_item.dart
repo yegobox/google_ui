0eeea6a198d35c4aaa1dba641ce5dc4f21eec2bc
Ed Sulaiman <52323183+edsulaiman@users.noreply.github.com>
2021-07-03 14:32:31 +0700

    Remove models

--- lib/src/models/google_popup_item.dart
+++
@@ -1,9 +0,0 @@
-class GooglePopupItem {
-  final void Function() onPressed;
-  final String label;
-
-  GooglePopupItem({
-    required this.onPressed,
-    required this.label,
-  });
-}
--- lib/src/models/index.dart
+++
@@ -1,3 +0,0 @@
-export 'google_list_item.dart';
-export 'google_menu_item.dart';
-export 'google_popup_item.dart';
