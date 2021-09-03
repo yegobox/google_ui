5b273d56ddd7a5ffd27b7e1172f3609d5412b9b6
Ed Sulaiman <52323183+edsulaiman@users.noreply.github.com>
2021-09-03 19:04:40 +0700

    Split widget file

--- lib/src/widgets/g_calendar_timeline/g_calendar_timeline.dart
+++ lib/src/widgets/g_calendar_timeline/g_calendar_timeline.dart
@@ -4,6 +4,7 @@ import 'package:intl/intl.dart';
 
 import 'g_calendar_timeline_controller.dart';
 import 'g_calendar_timeline_day.dart';
+import 'g_calendar_timeline_page_view.dart';
 
 /// Create a Calendar Timeline.
 class GCalendarTimeline extends HookWidget {
@@ -99,7 +100,7 @@ class GCalendarTimeline extends HookWidget {
           SizedBox(height: yearTextGap),
           SizedBox(
             height: height,
-            child: _GCalendarTimelinePageView(
+            child: GCalendarTimelinePageView(
               gap: gap,
               padding: padding,
               dayBuilder: dayBuilder,
@@ -134,109 +135,6 @@ class _YearText extends StatelessWidget {
     return Text(
       DateFormat("MMMM y").format(currentRowMonth),
       style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-    );
-  }
-}
-
-class _GCalendarTimelinePageView extends HookWidget {
-  const _GCalendarTimelinePageView({
-    Key? key,
-    required this.gap,
-    required this.padding,
-    required this.dateTimes,
-    required this.dayBuilder,
-    required this.controller,
-    required this.onDaySelected,
-    required this.onPageChanged,
-  }) : super(key: key);
-
-  final double gap;
-  final EdgeInsets padding;
-  final List<DateTime> dateTimes;
-  final Widget Function(
-    DateTime dateTime,
-    bool isToday,
-    bool isSelected,
-  )? dayBuilder;
-  final GCalendarTimelineController controller;
-  final void Function(DateTime value)? onDaySelected;
-  final void Function(int index)? onPageChanged;
-
-  @override
-  Widget build(BuildContext context) {
-    final selectedDay = useState(controller.selectedDay);
-
-    useEffect(() {
-      controller.addListener(() {
-        selectedDay.value = controller.selectedDay;
-      });
-    }, []);
-
-    void _onDaySelected(DateTime dateTime) {
-      controller.selectedDay = dateTime;
-      if (onDaySelected != null) {
-        onDaySelected!(dateTime);
-      }
-    }
-
-    return PageView.builder(
-      controller: controller.pageController,
-      itemCount: (controller.dateTimeLength / controller.dayInRow).ceil(),
-      itemBuilder: (context, index) {
-        final children = <Widget>[];
-        for (int i = 0; i < controller.dayInRow; i++) {
-          final dayInRow = controller.dayInRow;
-          final dateTimeLength = controller.dateTimeLength;
-          final now = controller.today;
-
-          final dateTimeIndex = index * dayInRow + i;
-          if (dateTimeIndex > dateTimeLength - 1) {
-            children.add(Expanded(child: Container()));
-            continue;
-          }
-
-          final dateTime = dateTimes[dateTimeIndex];
-          final dateNow = DateTime(
-            now.year,
-            now.month,
-            now.day,
-          );
-          final selectedDate = DateTime(
-            selectedDay.value.year,
-            selectedDay.value.month,
-            selectedDay.value.day,
-          );
-
-          children.add(
-            Expanded(
-              child: GestureDetector(
-                onTap: () => _onDaySelected(dateTime),
-                child: dayBuilder == null
-                    ? GCalendarTimelineDay(
-                        dateTime: dateTimes[dateTimeIndex],
-                        isToday: dateNow == dateTime,
-                        isSelected: selectedDate == dateTime,
-                      )
-                    : dayBuilder!(
-                        dateTimes[dateTimeIndex],
-                        dateNow == dateTime,
-                        selectedDate == dateTime,
-                      ),
-              ),
-            ),
-          );
-
-          if (i != controller.dayInRow - 1) {
-            children.add(SizedBox(width: gap));
-          }
-        }
-
-        return Padding(
-          padding: EdgeInsets.fromLTRB(padding.left, 0, padding.right, 0),
-          child: Row(children: children),
-        );
-      },
-      onPageChanged: onPageChanged,
     );
   }
 }
