93b8f21844432911170c5d9190470c55f71930fb
Ed Sulaiman <52323183+edsulaiman@users.noreply.github.com>
2021-09-26 12:25:21 +0700

    Add docs for GResponsive

--- lib/src/g_responsive.dart
+++ lib/src/g_responsive.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/cupertino.dart';
 
+/// An utility that responsible for responsive view
 class GResponsive {
+  /// Set breakpoint value for small screen
   static double smallBreakpoint = 640;
+
+  /// Set breakpoint value for medium screen
   static double mediumBreakpoint = 1008;
 
+  /// Return T value based on screen width
   static T on<T>(
     BuildContext context, {
     required T initial,
@@ -18,16 +23,20 @@ class GResponsive {
     return large ?? initial;
   }
 
+  /// Return true if screen width lower than or equals to [smallBreakpoint]
   static bool isSmall(BuildContext context) {
     final screenSize = MediaQuery.of(context).size.width;
     return screenSize <= smallBreakpoint;
   }
 
+  /// Return true if screen width bigger than [smallBreakpoint] and
+  /// lower than or equals to [mediumBreakpoint]
   static bool isMedium(BuildContext context) {
     final screenSize = MediaQuery.of(context).size.width;
     return screenSize > smallBreakpoint && screenSize <= mediumBreakpoint;
   }
 
+  /// Return true if screen width bigger than [mediumBreakpoint]
   static bool isLarge(BuildContext context) {
     final screenSize = MediaQuery.of(context).size.width;
     return screenSize > mediumBreakpoint;
