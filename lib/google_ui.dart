b408c77e8b8c7f79e943d2266bb3161365c44b86
Ed Sulaiman <52323183+edsulaiman@users.noreply.github.com>
2021-10-10 13:41:09 +0700

    Remove GImage

--- lib/google_ui.dart
+++ lib/google_ui.dart
@@ -19,7 +19,6 @@ export 'src/widgets/g_error_message/g_error_message.dart';
 export 'src/widgets/g_grid/g_grid.dart';
 export 'src/widgets/g_grid_builder/g_grid_builder.dart';
 export 'src/widgets/g_grouped_drawer/g_grouped_drawer.dart';
-export 'src/widgets/g_image/g_image.dart';
 export 'src/widgets/g_list_tile/g_list_tile.dart';
 export 'src/widgets/g_outlined_button/g_outlined_button.dart';
 export 'src/widgets/g_popup_menu_button/g_popup_menu_button.dart';
